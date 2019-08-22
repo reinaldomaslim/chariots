7e1058f70d0ea3b93bc8f97bbc2a1ffa3dc20b62
Ubuntu <ubuntu@ip-172-26-13-114.ap-southeast-1.compute.internal>
2019-08-22 15:12:33 +0000

    try

--- launch_web.sh
+++ launch_web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ./venv/bin/activate
 gcloud iam service-accounts list --project  chariots-dbig  --format="value(reinaldomaslim)"
-gcloud app deploy --quiet --project chariots-dbig
+gcloud app deploy --project chariots-dbig
